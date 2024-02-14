f9502d04c2814629f46760712ef1b53d6833687e
EvaMaeRey <evangeline.mae@gmail.com>
2024-02-14 14:32:17 -0700

    just giving this another try on 14 Feb

--- app/server.R
+++ app/server.R
@@ -36,17 +36,17 @@ return_heart_df <- function(n_vertices){
 
 
 ### Design code to be featured in app ####
-return_heart_df(n_vertices = 16) %>%
-  ggplot() +
-  aes(x = x, y = y, group = group) +
-  geom_polygon(
-    fill = "darkred",
-    color = "magenta",
-    linewidth = 4,
-    alpha = 0.8,
-    linetype = "dashed"
-  ) +
-  coord_equal()
+# return_heart_df(n_vertices = 16) %>%
+#   ggplot() +
+#   aes(x = x, y = y, group = group) +
+#   geom_polygon(
+#     fill = "darkred",
+#     color = "magenta",
+#     linewidth = 4,
+#     alpha = 0.8,
+#     linetype = "dashed"
+#   ) +
+#   coord_equal()
 
 
 #### version with variables predefined ###
@@ -287,65 +287,3 @@ server <- function(input, output) {
   
 }
 
-## Define UI for application that draws a heart ##
-ui <- fluidPage(
-  
-  # Application title
-  titlePanel("When your heart has some rough edges..." ),
-  
-  titlePanel("... add vertices!" ),
-  
-  # Sidebar with a slider input for number of bins
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput(inputId = "num_vertices",
-                  label = "vertices",
-                  step = 1,
-                  min = 10,
-                  max = 200,
-                  value = 16
-      ),
-      selectInput(inputId = "char_color",
-                  label = "color",
-                  selected = "magenta",
-                  choices = colors()
-      ),
-      selectInput(inputId = "char_fill",
-                  label = "fill",
-                  selected = "darkred",
-                  choices = colors(),
-      ),
-      radioButtons(inputId = "char_linetype",
-                   label = "linetype",
-                   selected = "dashed",
-                   choices = c("dashed", "dotted", "solid")),
-      sliderInput(inputId = "num_alpha",
-                  label = "alpha",
-                  value = .8,
-                  min = 0,
-                  max = 1,
-                  step = .02
-      ),
-      sliderInput(inputId = "num_linewidth",
-                  label = "linewidth",
-                  value = 4,
-                  min = 1,
-                  max = 5,
-                  step = 1
-      )
-      
-    ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-      verbatimTextOutput("distText"),
-      plotOutput("distPlot")
-    )
-    
-  )
-  
-  
-  # titlePanel(x_mod)
-  
-  
-)
